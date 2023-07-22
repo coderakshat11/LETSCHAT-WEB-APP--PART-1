6db6da0be312a1312dddd854456be77988bef7f8
coderakshat11 <123174715+coderakshat11@users.noreply.github.com>
2023-07-22 19:21:21 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBKec7Bh-ZaVZPn4xPiVJRpsblIOGHETlc",
+    authDomain: "chat-app-2-9b6e4.firebaseapp.com",
+    databaseURL: "https://chat-app-2-9b6e4-default-rtdb.firebaseio.com",
+    projectId: "chat-app-2-9b6e4",
+    storageBucket: "chat-app-2-9b6e4.appspot.com",
+    messagingSenderId: "920924623856",
+    appId: "1:920924623856:web:e80fde738865c2e7ca8335"
+  };
+  
+  // Initialize Firebase
+   firebase.initializeApp(firebaseConfig);
